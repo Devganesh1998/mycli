f36d6525068b01c99a6bf46e9b978ce65751f706
devganesh1998 <devganesh91@gmail.com>
2023-04-02 20:13:03 +0530

    haproxy status and start utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { platform } from 'os';
+import { execSync } from 'child_process';
 import { InvalidArgumentError } from 'commander';
 import fs from 'fs';
 import { DOCXN_SERVICE_ROUTE_CONFIG_FILE, T_NODE_SERVICE_STATUS, NODE_SERVICES } from './constants';
@@ -7,6 +9,10 @@ export const getArgumentValidator = (possibleValues: string[]) => (value: string
         throw new InvalidArgumentError(`Possible values - ${possibleValues.join(', ')}.`);
     }
     return value;
+};
+
+export const logToConsole = (message: string) => {
+    console.log(`\nmycli: ${message}.`);
 };
 
 export const updateThaproxyNodeServiceRoute = ({
@@ -40,5 +46,63 @@ export const updateThaproxyNodeServiceRoute = ({
 
         default:
             break;
+    }
+};
+
+export const getIsTracxnHaproxyUp = () => {
+    try {
+        execSync(
+            `
+        PORT=80
+        HOST="127.0.0.1"
+        {
+            echo "" > /dev/tcp/$HOST/$PORT && echo "Port $PORT is open"
+        } &> /dev/null
+        if [[ $? == 0 ]]; then
+            echo "\nmycli: Haproxy is running at \${HOST}:\${PORT}."
+            exit 0;
+        else
+            echo "\nmycli: Haproxy is not running at \${HOST}:\${PORT}."
+            exit 1;
+        fi
+    `,
+            {
+                encoding: 'utf8',
+                stdio: [0, 1, 2],
+            },
+        );
+        return true;
+    } catch (error) {
+        /* empty */
+    }
+    return false;
+};
+
+export const startTracxnHaproxy = () => {
+    try {
+        const osType = platform();
+        switch (osType) {
+            case 'darwin': {
+                execSync('brew services start haproxy', {
+                    encoding: 'utf8',
+                    stdio: [0, 1, 2],
+                });
+                break;
+            }
+
+            case 'linux': {
+                execSync('sudo service haproxy start', {
+                    encoding: 'utf8',
+                    stdio: [0, 1, 2],
+                });
+                break;
+            }
+
+            default:
+                break;
+        }
+    } catch (error: any) {
+        logToConsole(`Error in starting haproxy, message: ${error?.message || ''}`);
+        logToConsole('Please start haproxy by using `docxn haproxy start`');
     }
 };
